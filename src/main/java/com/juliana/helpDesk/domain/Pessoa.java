38ca7473d024c52cb291857674a197ce462afcb2
JuOliver <juoliverpt@gmail.com>
2022-06-01 09:53:32 -0400

    anotações JPA para classes de dominio

--- src/main/java/com/juliana/helpDesk/domain/Pessoa.java
+++ src/main/java/com/juliana/helpDesk/domain/Pessoa.java
@@ -1,20 +1,43 @@
 package com.juliana.helpDesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.juliana.helpDesk.domain.enums.Perfil;
 
-public abstract class Pessoa {
+@Entity
+public abstract class Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	@Column(unique = true)
 	protected String cpf;
+	@Column(unique = true)
+	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
 	public Pessoa() {
@@ -22,11 +45,12 @@ public abstract class Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
-	public Pessoa(Integer id, String nome, String cpf, String senha) {
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
+		this.email = email;
 		this.senha = senha;
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -49,6 +73,16 @@ public abstract class Pessoa {
 
 	public String getCpf() {
 		return cpf;
+	}
+	
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public void setCpf(String cpf) {
