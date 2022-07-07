4ce6e3385ccc18ac4fc975806bfca41aec3bdec0
Juliana <juoliverpt@gmail.com>
2022-07-06 22:36:43 -0400

    endpoint findAll para tecnico

--- src/main/java/com/juliana/helpDesk/resources/TecnicoResource.java
+++ src/main/java/com/juliana/helpDesk/resources/TecnicoResource.java
@@ -1,5 +1,9 @@
 package com.juliana.helpDesk.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,4 +31,10 @@ public class TecnicoResource {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDto = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 }
