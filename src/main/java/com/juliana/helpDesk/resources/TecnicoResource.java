097e245a5b280328d841db1870d1ef649c21b123
Juliana <juoliverpt@gmail.com>
2022-07-11 17:46:48 -0400

    metodo put para tecnicos implementado

--- src/main/java/com/juliana/helpDesk/resources/TecnicoResource.java
+++ src/main/java/com/juliana/helpDesk/resources/TecnicoResource.java
@@ -2,17 +2,16 @@ package com.juliana.helpDesk.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +49,11 @@ public class TecnicoResource {
 		Tecnico newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri(); 
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO){
+		Tecnico obj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
