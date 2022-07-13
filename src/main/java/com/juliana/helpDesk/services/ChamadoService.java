ed46195d0f2a53f1aa61413b677399ee7c46e7bd
Juliana <juoliverpt@gmail.com>
2022-07-13 17:11:10 -0400

    FindAll para chamado

--- src/main/java/com/juliana/helpDesk/services/ChamadoService.java
+++ src/main/java/com/juliana/helpDesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.juliana.helpDesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,13 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado!" + id));
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado!" + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 }
