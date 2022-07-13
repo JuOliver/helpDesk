106d7b33c729ef97855d315e93b0fa8a2e529318
Juliana <juoliverpt@gmail.com>
2022-07-13 16:21:25 -0400

    FindById para chamado

---
+++ src/main/java/com/juliana/helpDesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.juliana.helpDesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juliana.helpDesk.domain.Chamado;
+import com.juliana.helpDesk.repositories.ChamadoRepository;
+import com.juliana.helpDesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado!" + id));
+	}
+}
